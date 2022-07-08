edc4740e4929e6beea5e0202342c7058f098d8d8
Nyein Thiha Zaw <104121152+nyeinthihazaw@users.noreply.github.com>
2022-07-08 23:44:05 +0630

    Update air_pollution.py

--- air_pollution.py
+++ air_pollution.py
@@ -45,7 +45,7 @@ elif selected == "Test":
         uploaded_file = st.file_uploader("Choose a file...")
 
         if uploaded_file is not None:
-          csv_file= pd.read_csv(uploaded_file, colums=['Air Pollution Prediction'])
+          csv_file= pd.read_csv(uploaded_file)
 
           #change wind_dir and del previous one
           def func(s):
@@ -84,10 +84,10 @@ elif selected == "Test":
           result=result*std_op + mean_op
 
           #graph output
-#           chart_data = pd.DataFrame(
-#           result,
-#           colums=['Air Pollution Prediction'])
-          st.line_chart(result)
+          chart_data = pd.DataFrame(
+          result,
+          colums=['Air Pollution Prediction'])
+          st.line_chart(chart_data)
 #           from bokeh.plotting import figure
 
 #           x = []
